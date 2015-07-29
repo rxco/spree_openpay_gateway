e0354d1994a6d0f002523154b3d842c52bb70568
emsedano <elias.martinez@amplemind.com>
2015-07-29 00:01:34 -0500

    [merging open pay js class into Spree ctx]

--- app/assets/javascripts/spree/frontend/spree_openpay_gateway.js
+++ app/assets/javascripts/spree/frontend/spree_openpay_gateway.js
@@ -9,49 +9,98 @@
 
 ( function( window, undefined ) {
   
-  var _SpreeOpenpay = {
+  // closure js class 
+  var _SpreeOpenpay = function(f, g ) {
+
+    this._form = f;
+    this._gatewayOpts = g;    
+
+    this.methods       = this._form.find('input[name="order[payments_attributes][][payment_method_id]"]');
+    this.currentMethod = this.methods.filter(':checked').val();
     
-    initClient: function (id, k, isSandbox) {
-            
-      if(isSandbox !== undefined)
-      	OpenPay.setSandboxMode(isSandbox);
-      else 
-      	OpenPay.setSandboxMode(true);
+    this.listenChanges();    
+    this.listenForm();
+    console.log('OpenPay client initialized');
+  }
 
-     	OpenPay.setId(id);
-      OpenPay.setApiKey(k);
-    },
+  // privileged methods
+  _SpreeOpenpay.prototype = {
     
-
-    success_callbak: function(response) {
-        
-        _SpreeOpenpay.setOpenpayResponse(response);
-        _SpreeOpenpay.submitForm();
-
+    success_callbak: function(response) { 
+      console.log('success OpenPay callback function');
+      this.setOpenpayResponse(response);
+      this.submitForm();
     },
 
     error_callbak: function(response) {
      var desc = response.data.description != undefined ? 
-        response.data.description : response.message;
-		     alert("ERROR [" + response.status + "] " + desc);
-		     $("#pay-button").prop("disabled", false);
+      response.data.description : response.message;
+		  alert("ERROR [" + response.status + "] " + desc);
+		  $("#pay-button").prop("disabled", false);
 		},
 
     setOpenpayResponse: function(response) {
+      console.log('OpenPay response parsed and merged');
       var token_id = response.data.id;
       $('#token_id').val(token_id);
-      $('#checkout_form_payment').children("input[name='payment_source[#{@currentMethod}][gateway_payment_profile_id]']").val(response.id)
-      $('#checkout_form_payment').children("input[name='payment_source[#{@currentMethod}][openpay_response]']").val(JSON.stringify(response))
+      
+      this._form.children("input[name='payment_source["+this.currentMethod+"][gateway_payment_profile_id]']").val(response.id)
+      this._form.children("input[name='payment_source["+this.currentMethod+"][openpay_response]']").val(JSON.stringify(response))
     },
 
     submitForm: function() {
-      $('#checkout_form_payment').off('submit')
-      $('#checkout_form_payment').submit();
+      console.log('submitting Payment form');
+      this._form.off('submit')
+      this._form.submit();
+
+    },
+
+    listenForm: function(){
+      var that = this;
+      this._form.on('submit', function (e) {
+        e.preventDefault()
+        currentForm = that._form.clone();
+        
+        if (that.isConektaForm(currentForm) ) 
+          processOpenpayPayment(currentForm);
+        else
+          that.submitForm();
+
+      });
+    },    
+
+    isConektaForm: function (form) {
+      $('input', form).is("[data-openpay='card[name]']");
+    },
+
+    processOpenpayPayment: function(form) {
+      console.log('processing Openpay request');
+      $("#pay-button").prop( "disabled", true);
+      OpenPay.token.extractFormAndCreate('checkout_form_payment', success_callbak, error_callbak);
+    },
+
+    listenChanges: function(){
+      var that = this;
+      this.methods.on('change', function(e){
+        that.currentMethod = e.target.value;
+      });
+      
     }
     
+  };
+
+  // static methods
+  _SpreeOpenpay.configureAPI_Client = function (id, k, isSandbox) {
+            
+      isSandbox = isSandbox !== undefined ? isSandbox : true; //set default true
+      
+      OpenPay.setId(id);
+      OpenPay.setApiKey(k);
+      OpenPay.setSandboxMode(isSandbox);
+      console.log('OpenPay API client configured');
   }
-  
+
   // expose access to the constructor
-  window.SpreeOpenpay = _SpreeOpenpay;
+  window.Spree.Openpay = _SpreeOpenpay;
   
 } )( window );
